9088090c09b495767dfa737a6c757fa0971127c7
soby <vthquddbsv@gmail.com>
2024-12-10 22:45:04 +0900

    상속 클래스구현

---
+++ src/chapter5/ch01/CustomerTest.java
@@ -0,0 +1,18 @@
+package chapter5.ch01;
+
+public class CustomerTest {
+    public static void main(String[] args) {
+
+        Customer customerLee = new Customer();
+        customerLee.setCutomerName("이순신");
+        customerLee.setCutomerId(10010);
+        customerLee.bonusPoint = 1000;
+        System.out.println(customerLee.showCustomerInfo());
+
+        VIPCustomer customerKim = new VIPCustomer();
+        customerKim.setCutomerName("김유신");
+        customerKim.setCutomerId(10020);
+        customerKim.bonusPoint=10000;
+        System.out.println(customerKim.showCustomerInfo());
+    }
+}
---
+++ src/chapter5/ch01/VIPCustomer.java
@@ -0,0 +1,17 @@
+package chapter5.ch01;
+
+public class VIPCustomer extends Customer {
+
+    private int agentID;
+    double salesRatio;
+
+    public VIPCustomer(){
+        customerGrade = "VIP";
+        salesRatio = 0.05;
+        bonusRatio = 0.1;
+    }
+    public int getAgentID(){
+        return agentID;
+    }
+
+}
