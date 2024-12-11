a8990c99a55dee3fecd76b25d6afe2149139811a
soby <vthquddbsv@gmail.com>
2024-12-11 20:55:24 +0900

    다운캐스팅, instanceof

--- src/chapter5/ch06/AnimalTest.java
+++ src/chapter5/ch06/AnimalTest.java
@@ -12,17 +12,28 @@ class Human extends Animal {
     public void move() {
         System.out.println("사람이 두발로걷습니다.");
     }
+    public void readBooks(){
+        System.out.println("사람이 책을 읽습니다.");
+    }
 }
 
 class Tiger extends Animal {
     public void move() {
         System.out.println("호랑이가 네발로 뜁니다.");
     }
+
+    public void hunting() {
+        System.out.println("호랑이가 사냥을합니다");
+    }
 }
 
 class Eagle extends Animal {
     public void move() {
         System.out.println("독수리가 하늘을 날아갑니다.");
+    }
+
+    public void flying() {
+        System.out.println("독수리가 날개를 펴고 날아갑니다.");
     }
 }
 
@@ -45,12 +56,30 @@ public class AnimalTest {
         animalMove(hAnimal);
         animalMove(tAnimal);
         animalMove(eAnimal);
+        AnimalTest test = new AnimalTest();
+        test.testDownCasting(animalList);
     }
 
     public static void animalMove(Animal animal){
         animal.move();
     }
 
-//
+//다운 캐스팅
+    public static void testDownCasting(ArrayList<Animal> animalList) {
+        for (int i = 0; i < animalList.size(); i++) {
+            Animal animal = animalList.get(i);
+            if (animal instanceof Human) {
+                Human human = (Human) animal;
+                human.readBooks();
+            } else if (animal instanceof Tiger) {
+                Tiger tiger = (Tiger) animal;
+                tiger.hunting();
+            } else if (animal instanceof Eagle) {
+                Eagle eagle = (Eagle) animal;
+                eagle.flying();
+            }
 
+        }
+
+    }
 }
