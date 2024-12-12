8c82e0cbb5a89675b79ba87756c1bf520d181a0d
soby <vthquddbsv@gmail.com>
2024-12-12 14:24:06 +0900

    interface 요소들 추상메서드,정적메서드,private

---
+++ src/chapter5/ch14/Calc.java
@@ -0,0 +1,37 @@
+package chapter5.ch14;
+
+public interface Calc {
+
+    public static final double PI = 3.14;
+    int Error = -9999999;
+
+   public abstract int add(int num1, int num2);
+    int substract(int num1, int num2);
+    int times(int num1, int num2);
+    int divide(int num1, int num2);
+
+    default void description() {
+        myMethod();
+        mystaticMethod();
+        System.out.println("정수를 계산합니다. ");
+    }
+
+    private void myMethod() {
+        System.out.println("private method");
+    }
+
+    static int total(int[] arr) {
+        mystaticMethod();
+        int total = 0;
+
+        for(int i: arr) {
+            total += i;
+        }
+
+        return total;
+    }
+
+    private static void mystaticMethod() {
+        System.out.println("private static method");
+    }
+}
---
+++ src/chapter5/ch14/Calculator.java
@@ -0,0 +1,14 @@
+package chapter5.ch14;
+
+abstract class Calculator implements Calc {
+
+    @Override
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public int substract(int num1, int num2) {
+        return num1 - num2;
+    }
+}
